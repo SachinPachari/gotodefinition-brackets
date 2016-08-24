2d3008828c3324c623fb63dc09aae54788d0e3a9
Pachari Surendranath Sachin <sachin.ps@fujixerox.com>
2016-08-24 15:39:36 +0800

    Sample implementations of the feature. * Currently using the contextMenu(Right click) for user access. * Error Display during failure, is still pending.

--- main.js
+++ main.js
@@ -5,64 +5,162 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CommandManager  = brackets.getModule("command/CommandManager"),
-        EditorManager   = brackets.getModule("editor/EditorManager"),
-        Menus           = brackets.getModule("command/Menus"),
-        LanguageManager = brackets.getModule("language/LanguageManager"),
-        PerfUtils       = brackets.getModule("utils/PerfUtils"),
-        JSUtils         = brackets.getModule("language/JSUtils"),
-        ProjectManager  = brackets.getModule("project/ProjectManager");
+    var CommandManager          = brackets.getModule("command/CommandManager"),
+        Menus                   = brackets.getModule("command/Menus"),
+        EditorManager           = brackets.getModule("editor/EditorManager"),
+        KeyBindingManager       = brackets.getModule("command/KeyBindingManager"),
+        LanguageManager         = brackets.getModule("language/LanguageManager"),
+        JSUtils                 = brackets.getModule("language/JSUtils"),
+        FileViewController      = brackets.getModule("project/FileViewController"),
+        ProjectManager          = brackets.getModule("project/ProjectManager"),
+        storageStack            = [],
+        FUNCTION_NAME_INVALID   = "Invalid Function Name",
+        NO_DEFINITION_MATCH     = "No Definition match";
 
 
     // Function to run when the menu item is clicked
-    function handleHelloWorld() {
+    function handleGoToForward() {
+        
         var editor = EditorManager.getFocusedEditor();
-        if (editor !== undefined && isValidEditorText(editor)) {
-            var insertionPos = editor.getCursorPos();
-            var selectedText = editor.getSelectedText();
-//            _findInProject();
-            _findInProject(selectedText).done(function (functions) {
-                if (functions && functions.length > 0) {
-                    var jsInlineEditor = new MultiRangeInlineEditor(functions);
-                    jsInlineEditor.load(hostEditor);
-
-                    PerfUtils.addMeasurement(PerfUtils.JAVASCRIPT_INLINE_CREATE);
-                    result.resolve(jsInlineEditor);
-                } else {
-                    // No matching functions were found
-                    PerfUtils.addMeasurement(PerfUtils.JAVASCRIPT_INLINE_CREATE);
-                    result.reject();
-                }
-            }).fail(function () {
-                PerfUtils.finalizeMeasurement(PerfUtils.JAVASCRIPT_INLINE_CREATE);
-                result.reject();
-            });
-            editor.document.replaceRange("Hello, world!", insertionPos);
+        // Only provide a JavaScript editor when cursor is in JavaScript content
+        if (editor.getModeForSelection() !== "javascript") {
+            return null;
+        }
+        
+        // Only provide JavaScript editor if the selection is within a single line
+        var sel = editor.getSelection();
+        if (sel.start.line !== sel.end.line) {
+            return null;
+        }
+        
+        var selectedText = _getFunctionName(editor, sel.start);
+        if (selectedText.functionName !== null) {
+            storageStack.push({file: editor.getFile(), pos: sel});
+//            var selectedText = editor.getSelectedText();
             
+            findFunctionFile(selectedText.functionName).done(function () {
+//                editor.document.replaceRange("Hello, world!", insertionPos);    
+                console.debug(arguments);
+            }).fail(function () {
+                console.debug(arguments);
+            });
+        }else{
+            _handleInvalid(selectedText.reason);
         }
     }
     
-    function isValidEditorText (editor) {
-        var flag = true;
-        var selection = editor.getSelection();
-        var selectedText = editor.getSelectedText();
-        
-        if(selectedText === undefined && selectedText === ""){
-            flag = false;
-            return flag;
+    function handleGoToBack() {
+        if(storageStack.length > 0){
+            var memory = storageStack.pop();
+            var filePath = memory.file._path;
+            var ch = memory.pos.end.ch;
+            var line = memory.pos.end.line;
+            FileViewController.openFileAndAddToWorkingSet(filePath).done(function () {
+                EditorManager.getCurrentFullEditor().setCursorPos(line, ch, true);
+            });
         }
-        
-        if(selection.start.line !== selection.end.line){
-            flag = false;
-            return flag;
+    } 
+    
+    
+    /**
+     * Return the token string that is at the specified position.
+     *
+     * @param hostEditor {!Editor} editor
+     * @param {!{line:number, ch:number}} pos
+     * @return {functionName: string, reason: string}
+     */
+    function _getFunctionName(hostEditor, pos) {
+        var token = hostEditor._codeMirror.getTokenAt(pos, true);
+
+        // If the pos is at the beginning of a name, token will be the
+        // preceding whitespace or dot. In that case, try the next pos.
+        if (!/\S/.test(token.string) || token.string === ".") {
+            token = hostEditor._codeMirror.getTokenAt({line: pos.line, ch: pos.ch + 1}, true);
         }
-        return flag;
+
+        // Return valid function expressions only (function call or reference)
+        if (!((token.type === "variable") ||
+              (token.type === "variable-2") ||
+              (token.type === "property"))) {
+            return {
+                functionName: null,
+                reason: FUNCTION_NAME_INVALID
+            };
+        }
+
+        return {
+            functionName: token.string,
+            reason: null
+        };
     }
     
-    function _findInProject(functionName) {
+    function findFunctionFile (functionName) {
+        
+        var helper = brackets._jsCodeHintsHelper;
+        if (helper === null) {
+            return null;
+        }
+
         var result = new $.Deferred();
 
-        PerfUtils.markStart(PerfUtils.JAVASCRIPT_FIND_FUNCTION);
+        var response = helper();
+        if (response.hasOwnProperty("promise")) {
+            response.promise.done(function (jumpResp) {
+                var resolvedPath = jumpResp.fullPath;
+                if (resolvedPath) {
+
+                    // Tern doesn't always return entire function extent.
+                    // Use QuickEdit search now that we know which file to look at.
+                    var fileInfos = [];
+                    fileInfos.push({name: jumpResp.resultFile, fullPath: resolvedPath});
+                    JSUtils.findMatchingFunctions(functionName, fileInfos, true)
+                        .done(function (functions) {
+                            if (functions && functions.length > 0) {
+                                // TODO - issue with setting the ch value. so the ursor will be in ch:0
+                                var cursorPos = {line: functions[0].lineStart};
+                                _setCursorPosition(cursorPos);
+                                result.resolve(functions);
+                            } else {
+                                // No matching functions were found
+                                result.reject();
+                            }
+                        })
+                        .fail(function () {
+                            result.reject();
+                        });
+
+                } else {        // no result from Tern.  Fall back to _findInProject().
+
+                    _findInProject(functionName).done(function (functions) {
+                        if (functions && functions.length > 0) {
+                            // TODO - issue with setting the ch value. so the ursor will be in ch:0
+                            var cursorPos = {line: functions[0].lineStart};
+                            _setCursorPosition(cursorPos);
+                            result.resolve(functions);
+                        } else {
+                            // No matching functions were found
+                            result.reject();
+                        }
+                    }).fail(function () {
+                        result.reject();
+                    });
+                }
+
+            }).fail(function () {
+                result.reject();
+            });
+
+        }
+
+        return result.promise();
+    }
+    
+    
+    /*
+    
+    */
+    function _findInProject(functionName) {
+        var result = new $.Deferred();
 
         function _nonBinaryFileFilter(file) {
             return !LanguageManager.getLanguageForPath(file.fullPath).isBinary();
@@ -72,11 +170,17 @@ define(function (require, exports, module) {
             .done(function (files) {
                 JSUtils.findMatchingFunctions(functionName, files)
                     .done(function (functions) {
-                        PerfUtils.addMeasurement(PerfUtils.JAVASCRIPT_FIND_FUNCTION);
-                        result.resolve(functions);
+                        // TODO - need to open the correct file. 
+                        if(functions.length === 0){
+                            result.reject({reason: NO_DEFINITION_MATCH});
+                            return;
+                        }
+                        var filePath = functions[0].document.file._path
+                        FileViewController.openFileAndAddToWorkingSet(filePath).done(function () {
+                            result.resolve(functions);
+                        });
                     })
                     .fail(function () {
-                        PerfUtils.finalizeMeasurement(PerfUtils.JAVASCRIPT_FIND_FUNCTION);
                         result.reject();
                     });
             })
@@ -87,18 +191,34 @@ define(function (require, exports, module) {
         return result.promise();
     }
     
-    function handleInvalid () {
-        console.debug('handleInvalid');
+    /**
+     * Set the cursor in a specific Position
+     * 
+     * @param cursorPos {!{line:number, ch:number}}
+     */
+    function _setCursorPosition (cursorPos) {
+        EditorManager.getCurrentFullEditor().setCursorPos(cursorPos.line, cursorPos.ch, true);
+    }
+    
+    /**
+     * Show info regarding the failure
+     * 
+     * @param cursorPos {!{line:number, ch:number}}
+     */
+    function _handleInvalid (reason) {
+        console.debug('Failed to load the definitions : /n',reason);
     }
 
 
     // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "sachin.clicktogotodefinition";   // package-style naming to avoid collisions
-    CommandManager.register("clicktogotodefinition", MY_COMMAND_ID, handleHelloWorld);
+    var MY_COMMAND_ID = "sachin.clicktogotodefinition.forward";
+    CommandManager.register("Navigate To Definition", MY_COMMAND_ID, handleGoToForward);
+    var MY_COMMAND_ID_BACK = "sachin.clicktogotodefinition.backward";
+    CommandManager.register("Go Back", MY_COMMAND_ID_BACK, handleGoToBack);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-//    menu.addMenuItem(MY_COMMAND_ID);
+    // addign to context menu for now since ctrl+click is used for multiple cursor
+    var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+    contextMenu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-E");
+    contextMenu.addMenuItem(MY_COMMAND_ID_BACK, "Ctrl-Alt-W");
+
 });
